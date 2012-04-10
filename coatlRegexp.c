648414df38ecfed6cae9433acba8d1e6aed36385
Frederic Bonnet <fredericbonnet@free.fr>
2012-04-10 13:58:22 +0200

    First import of Henry Spencer's regexp package; for now, works on flat arrays of Col_Char

--- coatlRegexp.c
+++ coatlRegexp.c
@@ -10,3 +10,32 @@
  */
 
 #include "include/coatl.h"
+
+#include "re/regex.h"
+
+int 
+Coatl_TestRegexp(
+    Col_Word re, 
+    Col_Word s)
+{
+    regex_t r;
+    Col_WordData redata, sdata;
+    const Col_Char *rec, *sc;
+    regmatch_t match[1];
+    int result;
+
+    re = Col_NormalizeRope(re, COL_UCS4, 0, 1);
+    Col_GetWordInfo(re, &redata);
+    rec = (const Col_Char *) redata.string.data;
+    s = Col_NormalizeRope(s, COL_UCS4, 0, 1);
+    Col_GetWordInfo(s, &sdata);
+    sc = (const Col_Char *) sdata.string.data;
+
+    CoatlReCompile(&r, rec, Col_RopeLength(re), REG_ADVANCED);
+
+    result = CoatlReExec(&r, sc, Col_RopeLength(s), NULL, sizeof(match)/sizeof(*match), match, 0);
+
+    CoatlReFree(&r);
+
+    return (result == REG_OKAY);
+}
--- include/coatlRegexp.h
+++ include/coatlRegexp.h
@@ -7,4 +7,6 @@
 #ifndef _COATL_REGEXP
 #define _COATL_REGEXP
 
+EXTERN int Coatl_TestRegexp(Col_Word re, Col_Word s);
+
 #endif /* _COATL_REGEXP */
