5c479a686c48b4f8047fed1207d6ba2e4eef066a
Frederic Bonnet <fredericbonnet@free.fr>
2012-04-13 16:25:46 +0200

    Added compiled regexp word type Added regexp compile & execute procs/flags/result codes

--- coatlRegexp.c
+++ coatlRegexp.c
@@ -10,43 +10,195 @@
  */
 
 #include "include/coatl.h"
+#include "coatlInternal.h"
 
 #include "re/regex.h"
 
-int 
-Coatl_TestRegexp(
-    Col_Word re, 
-    Col_Word s)
+/*
+ * Prototypes for functions used only in this file.
+ */
+
+static Col_CustomWordSizeProc RegexpSizeProc;
+static Col_CustomWordFreeProc RegexpFreeProc;
+
+
+/*
+================================================================================
+Section: Regular Expressions
+================================================================================
+*/
+
+/*---------------------------------------------------------------------------
+ * Internal Variable: regexpWordType
+ *
+ *	Custom word type holding a regex_t structure.
+ *---------------------------------------------------------------------------*/
+
+static Col_CustomWordType regexpWordType = {
+    COL_CUSTOM,
+    "regexp",
+    RegexpSizeProc,
+    RegexpFreeProc,
+    NULL /* childrenProc */
+};
+
+/*---------------------------------------------------------------------------
+ * Internal Function: RegexpSizeProc
+ *
+ *	Regexp word type size proc. Follows <Col_CustomWordSizeProc> signature.	
+ *
+ * Argument:
+ *	word	- Custom word to get size for.
+ *
+ * Result:
+ *	The custom word size in bytes.
+ *
+ * See also:
+ *	<regexpWordType>
+ *---------------------------------------------------------------------------*/
+
+static size_t
+RegexpSizeProc(
+    Col_Word word)
 {
-    regex_t r;
-    regmatch_t match[1];
+    return sizeof(regex_t);
+}
+
+/*---------------------------------------------------------------------------
+ * Internal Function: RegexpFreeProc
+ *
+ *	Regexp word type cleanup proc. Follows <Col_CustomWordFreeProc>
+ *	signature.
+ *
+ * Argument:
+ *	word	- Custom word to cleanup.
+ *
+ * Side effects:
+ *	Calls regex_t cleanup procedure.
+ *
+ * See also: 
+ *	<regexpWordType>
+ *---------------------------------------------------------------------------*/
+
+static void
+RegexpFreeProc(
+    Col_Word word)
+{
+    Col_WordData data;
+    regex_t *re;
+
+    Col_GetWordInfo(word, &data);
+    ASSERT(data.custom.type == &regexpWordType);
+    re = (regex_t *) data.custom.data;
+
+    CoatlReFree(re);
+}
+
+/*---------------------------------------------------------------------------
+ * Function: Coatl_RegexpCompile
+ *
+ *	Compile a regular expression for later execution.
+ *
+ * Arguments:
+ *	string	- Regular expression to compile.
+ *	flags	- Compilation flags (see <Regular Expression Flags>).
+ *
+ * Result:
+ *	An error code (see <Regular Expression Error Codes>). Additionally:
+ *
+ *	rePtr	- Regular expression word if compilation was successful (i.e.
+ *		  result is <COATL_REGEXP_OKAY>).
+ *
+ * Side effects:
+ *	May allocate memory cells.
+ *
+ * See also:
+ *	<Coatl_RegexpExec>
+ *---------------------------------------------------------------------------*/
+
+int
+Coatl_RegexpCompile(
+    Col_Word string, 
+    int flags,
+    Col_Word *rePtr)
+{
+    Col_RopeIterator it;
     int result;
+    regex_t re;
+    void *data;
 
 #if 1
+    Col_RopeIterFirst(string, &it); 
+    result = CoatlReCompile(&re, it, Col_RopeIterLength(&it), flags);
+#else
+    {
+    Col_WordData sdata;
+    const Col_Char4 *s;
+
+    string = Col_NormalizeRope(string, COL_UCS4, 0, 1);
+    Col_GetWordInfo(string, &sdata);
+    result = CoatlReCompile(&re, (const Col_Char4 *) sdata.string.data, 
+	    Col_RopeLength(string), flags);
+    }
+#endif
+    if (result == REG_OKAY) {
+	*rePtr = Col_NewCustomWord(&regexpWordType, sizeof(re), &data);
+	*(regex_t *) data = re;
+    } else {
+	*rePtr = WORD_NIL;
+    }
+    return result;
+}
+
+/*---------------------------------------------------------------------------
+ * Function: Coatl_RegexpExec
+ *
+ *	Execute a previously compiled regular expression.
+ *
+ * Arguments:
+ *	re	- Compiled regular expression (result of <Coatl_RegexpCompile>).
+ *	string	- String to match against regular expression.
+ *	flags	- Execution flags (see <Regular Expression Flags>).
+ *
+ * Result:
+ *	An error code (see <Regular Expression Error Codes>).
+ *
+ * See also:
+ *	<Coatl_RegexpCompile>
+ *---------------------------------------------------------------------------*/
+
+int
+Coatl_RegexpExec(
+    Col_Word re,
+    Col_Word string,
+    int flags)
+{
+    Col_WordData data;
+    regex_t *rePtr;
     Col_RopeIterator it;
 
-    Col_RopeIterFirst(re, &it); 
-    CoatlReCompile(&r, it, Col_RopeLength(re), REG_ADVANCED);
+    Col_GetWordInfo(re, &data);
+    if (data.custom.type != &regexpWordType) {
+	Col_Error(COL_ERROR, "%x is not a regexp", re);
+	return 0;
+    }
 
-    Col_RopeIterFirst(s, &it); 
-    result = CoatlReExec(&r, it, Col_RopeLength(s), NULL, sizeof(match)/sizeof(*match), match, 0);
+    rePtr = (regex_t *) data.custom.data;
+
+    //TODO ranges
+#if 1
+    Col_RopeIterFirst(string, &it);
+    return CoatlReExec(rePtr, it, Col_RopeIterLength(&it), NULL, 0, NULL, 
+	    flags);
 #else
-    Col_WordData redata, sdata;
-    const Col_Char4 *rec, *sc;
+    {
+    Col_WordData sdata;
+    const Col_Char4 *s;
 
-    re = Col_NormalizeRope(re, COL_UCS4, 0, 1);
-    Col_GetWordInfo(re, &redata);
-    rec = (const Col_Char4 *) redata.string.data;
-    s = Col_NormalizeRope(s, COL_UCS4, 0, 1);
-    Col_GetWordInfo(s, &sdata);
-    sc = (const Col_Char4 *) sdata.string.data;
-
-    CoatlReCompile(&r, rec, Col_RopeLength(re), REG_ADVANCED);
-
-    result = CoatlReExec(&r, sc, Col_RopeLength(s), NULL, sizeof(match)/sizeof(*match), match, 0);
+    string = Col_NormalizeRope(string, COL_UCS4, 0, 1);
+    Col_GetWordInfo(string, &sdata);
+    return CoatlReExec(rePtr, (const Col_Char4 *) sdata.string.data, 
+	    Col_RopeLength(string), NULL, 0, NULL, flags);
+    }
 #endif
-
-    CoatlReFree(&r);
-
-    return (result == REG_OKAY);
 }
--- include/coatlRegexp.h
+++ include/coatlRegexp.h
@@ -7,6 +7,116 @@
 #ifndef _COATL_REGEXP
 #define _COATL_REGEXP
 
-EXTERN int Coatl_TestRegexp(Col_Word re, Col_Word s);
+
+/*
+================================================================================
+Section: Regular Expressions
+================================================================================
+*/
+
+/*---------------------------------------------------------------------------
+ * Constants: Regular Expression Flags
+ *
+ *	Flags used during compilation & execution of regular expressions.
+ *
+ *  COATL_REGEXP_BASIC		- BREs (convenience).
+ *  COATL_REGEXP_EXTENDED	- EREs.
+ *  COATL_REGEXP_ADVF		- Advanced features in EREs.
+ *  COATL_REGEXP_ADVANCED	- AREs (which are also EREs).
+ *  COATL_REGEXP_QUOTE		- No special characters, none.
+ *  COATL_REGEXP_ICASE		- Ignore case.
+ *  COATL_REGEXP_NOSUB		- Don't care about subexpressions.
+ *  COATL_REGEXP_EXPANDED	- Expanded format, white space & comments.
+ *  COATL_REGEXP_NLSTOP		- *'\n'* doesn't match *'.'* or *'[^ ]'*.
+ *  COATL_REGEXP_NLANCH		- *'^'* matches after *'\n'*, *'$'* before.
+ *  COATL_REGEXP_NEWLINE	- Newlines are line terminators.
+ *  COATL_REGEXP_EXPECT		- Report details on partial/limited matches.
+ *
+ * Note:
+ *	These match the constant values used by the internal regexp package.
+ *
+ * See also: 
+ *	<Coatl_RegexpCompile>, <Coatl_RegexpExec>
+ *---------------------------------------------------------------------------*/
+
+#define COATL_REGEXP_BASIC	000000
+#define COATL_REGEXP_EXTENDED	000001
+#define COATL_REGEXP_ADVF	000002
+#define COATL_REGEXP_ADVANCED	000003
+#define COATL_REGEXP_QUOTE	000004
+#define COATL_REGEXP_ICASE	000010
+#define COATL_REGEXP_NOSUB	000020
+#define COATL_REGEXP_EXPANDED	000040
+#define COATL_REGEXP_NLSTOP	000100
+#define COATL_REGEXP_NLANCH	000200
+#define COATL_REGEXP_NEWLINE	000300
+#define COATL_REGEXP_EXPECT	001000
+
+/*---------------------------------------------------------------------------
+ * Constants: Regular Expression Error Codes
+ *
+ *	Error codes used during compilation & execution of regular expressions.
+ *
+ *  COATL_REGEXP_OKAY		-  No errors detected.
+ *  COATL_REGEXP_NOMATCH	-  Failed to match.
+ *  COATL_REGEXP_BADPAT		-  Invalid regexp.
+ *  COATL_REGEXP_ECOLLATE	-  Invalid collating element.
+ *  COATL_REGEXP_ECTYPE		-  Invalid character class.
+ *  COATL_REGEXP_EESCAPE	-  Invalid escape \ sequence.
+ *  COATL_REGEXP_ESUBREG	-  Invalid backreference number.
+ *  COATL_REGEXP_EBRACK		-  Brackets [] not balanced.
+ *  COATL_REGEXP_EPAREN		-  Parentheses () not balanced.
+ *  COATL_REGEXP_EBRACE		-  Braces {} not balanced.
+ *  COATL_REGEXP_BADBR		-  Invalid repetition count(s).
+ *  COATL_REGEXP_ERANGE		-  Invalid character range.
+ *  COATL_REGEXP_ESPACE		-  Out of memory.
+ *  COATL_REGEXP_BADRPT		-  Quantifier operand invalid.
+ *  COATL_REGEXP_ASSERT		-  "can't happen" -- you found a bug.
+ *  COATL_REGEXP_INVARG		-  Invalid argument to regex function.
+ *  COATL_REGEXP_MIXED		-  Character widths of regex and string differ.
+ *  COATL_REGEXP_BADOPT		-  Invalid embedded option.
+ *  COATL_REGEXP_ETOOBIG	-  Nfa has too many states.
+ *
+ * Debugging and testing:
+ *  COATL_REGEXP_ATOI		- Convert error-code name to number.
+ *  COATL_REGEXP_ITOA		- Convert error-code number to name.
+ *
+ * Note:
+ *	These match the constant values used by the internal regexp package.
+ *
+ * See also: 
+ *	<Coatl_RegexpCompile>, <Coatl_RegexpExec>
+ *---------------------------------------------------------------------------*/
+
+#define COATL_REGEXP_OKAY	  0
+#define COATL_REGEXP_NOMATCH	  1
+#define COATL_REGEXP_BADPAT	  2
+#define COATL_REGEXP_ECOLLATE	  3
+#define COATL_REGEXP_ECTYPE	  4
+#define COATL_REGEXP_EESCAPE	  5
+#define COATL_REGEXP_ESUBREG	  6
+#define COATL_REGEXP_EBRACK	  7
+#define COATL_REGEXP_EPAREN	  8
+#define COATL_REGEXP_EBRACE	  9
+#define COATL_REGEXP_BADBR	 10
+#define COATL_REGEXP_ERANGE	 11
+#define COATL_REGEXP_ESPACE	 12
+#define COATL_REGEXP_BADRPT	 13
+#define COATL_REGEXP_ASSERT	 15
+#define COATL_REGEXP_INVARG	 16
+#define COATL_REGEXP_MIXED	 17
+#define COATL_REGEXP_BADOPT	 18
+#define COATL_REGEXP_ETOOBIG	 19
+#define COATL_REGEXP_ATOI	101
+#define COATL_REGEXP_ITOA	102
+
+/*
+ * Remaining declarations.
+ */
+
+EXTERN int		Coatl_RegexpCompile(Col_Word string, int flags, 
+			    Col_Word *rePtr);
+EXTERN int		Coatl_RegexpExec(Col_Word re, Col_Word string, 
+			    int flags);
 
 #endif /* _COATL_REGEXP */
